dd3f1a350eff8041cf1534af559c66feb0a8a7ae
vbertazzo <vbertazzo@gmail.com>
2022-01-11 22:57:35 -0300

    Add Booking module

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -3,7 +3,17 @@ defmodule Flightex.Bookings.Booking do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(date, origin, destination, user_id)
+      when is_struct(date, NaiveDateTime) do
+    booking_id = UUID.uuid4()
+
+    {:ok,
+     %__MODULE__{
+       id: booking_id,
+       complete_date: date,
+       local_origin: origin,
+       local_destination: destination,
+       user_id: user_id
+     }}
   end
 end
