012042af91cdd280fa2856517e6bd8e0f13e18b5
vbertazzo <vbertazzo@gmail.com>
2022-01-11 22:23:09 -0300

    Add User module

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,18 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(name, email, cpf) when is_bitstring(cpf) do
+    id = UUID.uuid4()
+
+    {:ok,
+     %__MODULE__{
+       id: id,
+       name: name,
+       email: email,
+       cpf: cpf
+     }}
   end
+
+  def build(_name, _email, cpf) when not is_bitstring(cpf), do: {:error, "Cpf must be a String"}
+  def build(_name, _email, _cpf), do: {:error, "Invalid Parameters"}
 end
